87a636e52e57c8098c147c2621c2f50e8abb5a10
Martín Di Paola <martinsebastian1553@outlook.com>
2025-05-01 12:17:46 -0300

    Hero component modified

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import { TextAnimate } from "../components/magicui/components/text-animate";
 
 export default function Hero() {
     return (
-        <section className="relative w-full h-screen overflow-hidden mx-auto">
+        <section className="relative w-full h-screen bg-black overflow-hidden mx-auto">
             <Vortex
                 backgroundColor="black"
                 className="flex flex-col items-center justify-center px-4 md:px-10 py-8 w-full h-full"
