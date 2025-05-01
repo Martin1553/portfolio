88fcc81d681ce6184a11c54081eafc59a8b88602
Martín Di Paola <martinsebastian1553@outlook.com>
2025-05-01 19:32:43 -0300

    cv file moved to public file

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -56,7 +56,7 @@ export default function Header() {
       icon: (
         <IconCloudDownload className="h-full w-full text-neutral-500 dark:text-neutral-300" />
       ),
-      href: "src/assets/cv/Martín Di Paola - Full Stack Web Developer.pdf", // o "/assets/cv.pdf" si lo pusiste en /public/assets
+      href: "/cv/Martin-Di-Paola-Full-Stack-Web-Developer.pdf", // o "/assets/cv.pdf" si lo pusiste en /public/assets
       download: true,
     },
     {
